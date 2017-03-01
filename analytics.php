9b00a4efb20b6ce7f8be15f7ca35439c814c3474
Benjamin COIFFARD <benjamin.coiffard@aleysia.com>
2017-03-01 19:05:50 +0100

    Suppression de retours inutils

--- analytics.php
+++ analytics.php
@@ -11,31 +11,24 @@
 	
 	// Build API instance
 		$WZStat = new WZStat();
-		echo 'Initialisation de la classe WZStat: OK<br>';
 		
 	// Get parameters
 		$WZStat->extractParameters();
-		echo 'Paramètres récupérés: OK<br>';
 		
 	// Check rules
 		$WZStat->applyRules();
-		echo 'Règles appliqués: OK<br>';
 		
 	// Check user
 		$WZStat->checkUser();
-		echo 'Vérification de l\'utilisateur: OK<br>';
 		
 	// Check errors
 		$WZStat->checkErrors();
-		echo 'Nombre d\'erreurs : 0<br>';
 		
 	// Connect to MongoDB
 		$WZStat->connectToMongoDB();
-		echo 'Connexion à la base Mongo: OK<br>';
 		
 	// Add document to database
 		$WZStat->sendAnalyticsToMongoBase();
-		echo 'Ajout du document dans la base Mongo: OK<br>';
 		
 		echo 'success';
 	
